88ac7d8669032cf949d72f63d8346746cd909682
Yasushi Tamura <yasushitamura@YasushinoMacBook-Air.local>
2022-09-01 11:21:21 +0900

    並列処理のサンプルコード追加

---
+++ src/dev49_1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func normal(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	normal("hello")
+}
