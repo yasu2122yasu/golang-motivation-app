7275589675a40b1168b13453d6b757d4424b1b7f
Yasushi Tamura <yasushitamura@YasushinoMacBook-Air.local>
2022-09-02 07:27:26 +0900

    fan-outとfan-inの動作追加

---
+++ src/dev51_2.go
@@ -0,0 +1,18 @@
+//Buffered Channels
+
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 2)
+	ch <- 100
+	fmt.Println(len(ch))
+	ch <- 200
+	fmt.Println(len(ch))
+	close(ch)
+
+	for c := range ch {
+		fmt.Println(c)
+	}
+}
