34f5828b5e5ba2aa37962a702fb19bb0ca89f7a4
Yasushi Tamura <yasushitamura@YasushinoMacBook-Air.local>
2022-09-03 16:27:24 +0900

    パッケージの追加

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module test-module
 
 go 1.18
-
-replace GoTo/mylib => ../mylib
--- src/dev49_1.go
+++ src/dev49_1.go
@@ -5,6 +5,9 @@ import (
 	"time"
 )
 
+/*
+時間差でHelloを出力し続ける。
+*/
 func normal(s string) {
 	for i := 0; i < 5; i++ {
 		time.Sleep(100 * time.Millisecond)
