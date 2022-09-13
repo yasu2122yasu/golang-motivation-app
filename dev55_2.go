c8508639d5d33aba81e1d6c4365e44b707424a87
Yasu <100980869+yasu2122yasu@users.noreply.github.com>
2022-09-13 23:55:06 +0900

    Update dev55_2.go

--- dev55_2.go
+++ dev55_2.go
@@ -10,14 +10,6 @@ func main() {
 	boom := time.After(500 * time.Millisecond)
 	for {
 		select {
-		case <-tick:
-			fmt.Println("tick.")
-		case <-boom:
-			fmt.Println("BOOM!.")
-			return
-		default:
-			fmt.Println("     .")
-			time.Sleep(50 * time.Millisecond)
 		}
 	}
 }
