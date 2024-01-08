6b3c53aa482c7fcd34a94ae48eb8b1158abf73d7
yasu2122yasu <ya2su1shi2@icloud.com>
2024-01-08 21:41:30 +0900

    make pull requeset

---
+++ frontend/app/src/pages/AfterLoginUserPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AfterLoginUserPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the After Login User Page!</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default AfterLoginUserPage;
---
+++ frontend/app/src/pages/LoginUserPage.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const LoginUserPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginUserPage;
